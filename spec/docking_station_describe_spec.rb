a82ceb05fa00e5e6219f942855366a8cb9f52a3c
Allan Wazacz <cazwazacz@gmail.com>
2017-10-25 13:48:48 +0100

    step12-fixed

--- spec/docking_station_describe_spec.rb
+++ spec/docking_station_describe_spec.rb
@@ -3,20 +3,23 @@ require 'bike'
 
 describe DockingStation do
 
-  station =   DockingStation.new
-  bike = station.release_bike
+  # station =   DockingStation.new
+  # bike = station.release_bike
+  #
+  # it "releases bike and is working" do
+  #   expect(station).to respond_to(:release_bike)
+  #   expect(bike.working?).to eq true
+  # end
+  #
+  # it "docks the bike" do
+  #   expect(station).to respond_to(:dock).with(1).argument
+  # end
 
-  it "releases bike and is working" do
-    expect(station).to respond_to(:release_bike)
-    expect(bike.working?).to eq true
-  end
-
-  it "docks the bike" do
-    expect(station).to respond_to(:dock).with(1).argument
-  end
-
-  it "raises" do
-    expect {station.release_bike}.to raise_error
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      # we want to release the bike we docked
+      expect{subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 
 end
